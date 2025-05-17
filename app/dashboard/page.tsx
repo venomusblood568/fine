d9bb979012054bb4b3e8a55d95c2e3ef25239816
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-05-17 09:32:39 +0530

    dashboard start

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="bg-noise min-h-screen flex items-center justify-center text-white">
+      <div>
+        <h1 className="text-4xl font-bold">Dashboard</h1>
+      </div>
+    </div>
+  );
+}

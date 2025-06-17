d35bfda4066a1094b5ac7f25232ff43b627923ed
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-06-17 21:00:33 +0530

    initial setup for fe

---
+++ fe/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-gray-500 min-h-screen flex w-full'>
+        <div className='flex'>
+          <h1 className='text-white'>Fe test setup</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App

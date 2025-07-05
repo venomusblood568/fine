32368531eebaf4aa84672da5968b3c06dc0fdc75
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-07-05 20:27:55 +0530

    fecth expense

--- fe/src/Components/small_components/expense.tsx
+++ fe/src/Components/small_components/expense.tsx
@@ -1,6 +1,50 @@
+import { useEffect,useState } from "react";
 
+type TransactionType = {
+  _id: string;
+  accountId: {
+    _id: string;
+    accountName: string;
+  };
+  type: string;
+  amount: number;
+  description: string;
+  toWhom: string;
+  date: string;
+};
 
 export default function Expense() {
+  const[transactions, setTransactions] = useState<TransactionType[]>([]);
+  const[loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchTransaction = async () =>{
+      const token = localStorage.getItem("token");
+      if(!token){
+        console.warn(`No token found`)
+        return
+      }
+      try {
+        const res = await fetch("http://localhost:3001/api/trans/get-trans", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!res.ok) {
+          throw new Error(`Fetch failed: ${res.status}`);
+        }
+
+        const data = await res.json();
+        setTransactions(data.transaction || []);
+      } catch (error) {
+        console.error(`Error fetching transactions:`, error);
+      }finally{
+        setLoading(false);
+      }
+    }
+    fetchTransaction()
+  },[])
   return (
     <div className="lg:col-span-5 bg-white/10 rounded-2xl p-4 col-span-1 shadow-sm backdrop-blur-sm  overflow-y-auto">
       {/* Header Row */}
@@ -16,15 +60,28 @@ export default function Expense() {
 
       {/* Scrollable Content */}
       <div className="flex-1 overflow-y-auto pr-1 text-sm text-gray-300 space-y-2">
-        {[...Array(30)].map((_, i) => (
-          <div
-            key={i}
-            className="flex justify-between  border-white/10 pb-1"
-          >
-            <span>Expense {i + 1}</span>
-            <span>$25</span>
-          </div>
-        ))}
+        {loading ? (
+          <p>Loading...</p>
+        ) : transactions.length === 0 ? (
+          <p>No transactions found.</p>
+        ) : (
+          transactions.map((tx) => (
+            <div
+              key={tx._id}
+              className="flex justify-between border-white/10 pb-1"
+            >
+              <div>
+                <p className="font-medium">{tx.description}</p>
+                <p className="text-xs text-gray-600">
+                  {tx.accountId.accountName} • {tx.type}
+                </p>
+              </div>
+              <span className={tx.type === "Income" ? "text-green-600": "text-red-600"}>
+                  ${tx.amount}
+              </span>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );

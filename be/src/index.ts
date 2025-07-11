5b7670cc0a4295bbbccbb8f31c0e4f9ac911ec40
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-07-11 19:55:30 +0530

    fix

--- be/src/index.ts
+++ be/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import apiRouter from "./routes";
 import "./models";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,15 +11,19 @@ const PORT = process.env.PORT || 3001;
 app.use(
   cors({
     origin: [
-      "https://fine-duck.vercel.app/",
+      "https://fine-duck.vercel.app",
+      "https://fine-p3kaxq0w0-venomusblood568s-projects.vercel.app",
     ],
     credentials: true,
   })
 );
-app.use(express.json())
 
-app.use("/api",apiRouter);
+// Optional: handle preflight requests
+app.options("*", cors());
+
+app.use(express.json());
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}/`);
-})
+  console.log(`Server running on http://localhost:${PORT}/`);
+});
